ee8acf01b712afd1396f84504bedd7cc35da2364
Justin Garner <charlottewebsolutions1@gmail.com>
2024-01-30 14:00:21 -0500

    Added fonts and contiuned main page

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+
+<div class="w-screen h-screen flex">
+    <div class="h-full w-1/2 gray-bg">
+    
+    </div>
+
+    <div class="h-full w-1/2 purple-bg flex flex-col pt-16 px-16 text-white">
+        <h2 class="text-xl font-medium">Coinbank</h2>
+
+        
+        <h1 class="mt-9 text-5xl">Create an account</h1>
+        
+        <p class="mt-2.5 text-gray-200 font-light text-lg">Let's begin your 30-day-risk-free trial.</p>
+        
+        
+        <form class="mt-10" action="">
+            <div class="w-5/6">
+                <label for="last-name" class="block leading-6 text-sm">Name</label>
+                <div class="mt-2">
+                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="bg-gray-100 pl-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+
+            <div class="mt-6 w-5/6">
+                <label for="last-name" class="block leading-6 text-sm">Email</label>
+                <div class="mt-2">
+                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="bg-gray-100 pl-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+
+            <div class="mt-6 w-5/6">
+                <label for="last-name" class="block leading-6 text-sm">Password</label>
+                <div class="mt-2">
+                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="bg-gray-100 pl-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            
+            <button type="submit" class="mt-14 w-5/6 rounded-md bg-light-purple py-3 font-medium text-gray-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </form>
+        
+
+    </div>
+</div>
+
+<style>
+    *{
+        font-family: Neurial-Grotesk
+    }
+
+    .bg-light-purple{
+        background: rgb(101, 73, 255)    
+    }
+
+    .gray-bg{
+        background: #fcfcfe;
+    }
+
+    .purple-bg{
+        background: #4011e2;
+        border-radius: 4% 0% 0% 4%;
+    }
+</style>
